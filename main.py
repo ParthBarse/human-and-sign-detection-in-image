c2f8ea57a3e9d496c8208f3c64b67b13aee5360b
Parth Barse <91686761+ParthBarse@users.noreply.github.com>
2022-10-01 10:34:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import cv2
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['POST', 'GET'])
+def home():
+    return "ok"
+
+
+@app.route('/detectHuman',methods = ['POST', 'GET'])
+def detectHuman():
+    img = request.args.get("imgUrl")
+    print(img)
+
+    imgUrl = img
+    response = requests.get(imgUrl)
+    file = open("sample_image.png", "wb")
+    file.write(response.content)
+    file.close()
+
+    # Get user supplied values
+    imagePath = "sample_image.png"
+    cascPath = "haarcascade_frontalface_default.xml"
+
+    # Create the haar cascade
+    faceCascade = cv2.CascadeClassifier(cascPath)
+
+    # Read the image
+    image = cv2.imread(imagePath)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+        )
+
+    if len(faces) >= 1:
+        return "Person"
+    else:
+        return "Signature"
